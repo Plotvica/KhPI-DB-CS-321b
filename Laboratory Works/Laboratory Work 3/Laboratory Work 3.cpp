3ffd5a3240498c3ec4171e621dddc72b4fa69202
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-10-05 23:07:43 +0300

    4

---
+++ Laboratory Works/Laboratory Work 3/Laboratory Work 3.cpp
@@ -0,0 +1,30 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h"
+#include "math.h"
+using namespace std;
+
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	/* параметры, которые вводятся */
+	double x;
+	/* параметры, которые задаются в программе */
+	double a = 12.5, b = 1.3;
+	double t1, t2;   /* результаты */
+	double ax;       /* рабочая переменная */
+
+	printf("Введите x>");
+	scanf("%lf", &x);
+
+	ax = a - b;
+	t1 = b/( ax*(a+x) ) - a/(ax*ax) * log((a+x)/(b+x));
+
+	ax = a * x;
+	t2 = (1/a)*(log(tan(ax/2))-1/(sin(ax)));
+
+	printf("t1 = %lg\n", t1);
+	printf("t2 = %lg\n", t2);
+
+	return 0;
+}
