399912cd01363d8e36bf70f9fec322e683619b48
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-10-06 15:03:14 +0300

    2

--- Laboratory Works/Laboratory Work 2/Laboratory Work 2.cpp
+++ Laboratory Works/Laboratory Work 2/Laboratory Work 2.cpp
@@ -10,20 +10,25 @@ int main()
 
 	
 
-	char name1[11], name2[11], name3[11]; 
+	/*char name1[11], name2[11], name3[11];
 	char type1, type2, type3;
 	int cnt1, cnt2, cnt3;
-	float wei1, wei2, wei3;
+	float wei1, wei2, wei3;*/
+
+	char name1[11] = "Фланць", name2[11] = "Перехідник", name3[11] = "Станина"; //strcpy(name1, "Тодайдзи");
+	char type1 = 'О', type2 = 'П', type3 = 'З';
+	int cnt1 = 3, cnt2 = 8, cnt3 = 1;
+	float wei1 = 450, wei2 = 74, wei3 = 117050;
 
 	 
-	printf("1. Введіть: назву, тип, кількість, вагу (г) >"); // фланець З 3 450
-	scanf("%s %c %d %f", name1, &type1, &cnt1, &wei1);
+	//printf("1. Введіть: назву, тип, кількість, вагу (г)  >"); // фланець З 3 450
+	//scanf("%s %c %d %f", name1, &type1, &cnt1, &wei1);
 
-	printf("2. Введіть: назву, тип, кількість, вагу (г) >"); // перехідник П 8 74
-	scanf("%s %c %d %f", name2, &type2, &cnt2, &wei2);
+	//printf("2. Введіть: назву, тип, кількість, вагу (г) > "); // перехідник П 8 74
+	//scanf("%s %c %d %f", name2, &type2, &cnt2, &wei2);
 
-	printf("3. Введіть: назву, тип, кількість, вагу (г) >"); // станина О 1 117050
-	scanf("%s %c %d %f", name3, &type3, &cnt3, &wei3);
+	//printf("3. Введіть: назву, тип, кількість, вагу (г) >"); // станина О 1 117050
+	//scanf("%s %c %d %f", name3, &type3, &cnt3, &wei3);
 
 
 
