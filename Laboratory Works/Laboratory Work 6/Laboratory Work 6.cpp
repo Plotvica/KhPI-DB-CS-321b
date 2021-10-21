707a4bc597a7c8d16976e49c2642150942db80b3
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-10-21 14:39:04 +0300

    1

--- Laboratory Works/Laboratory Work 6/Laboratory Work 6.cpp
+++ Laboratory Works/Laboratory Work 6/Laboratory Work 6.cpp
@@ -3,39 +3,33 @@
 #include "windows.h";
 using namespace std;
 
-int main()
-{
+int main(){
+
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     
-	long n;
+	double n;
 	double dbln;
-	double sum = 0;
+    double sum = 0;
 	double term;
-	const double lim = 0.000001; 
+	const double lim = 0.000001;
 	short k1 = 1;
-	//long k3 = 1, k2 = 1;
 	
-	for (n = 0; ; n++, k1 = -k1) 
+	for ( n=0; ; n++, k1 = -k1) 
 	{
 		dbln = n;
 		term = k1 * (n*n + 1) / (n*n*n + 3);
-		if (fabs(term) >= lim)
-		{ 
+		if (fabs(term) >= lim){ 
 			sum += term;
 		}
-		else 
-		{
+		else{ 
 			break;
 		}
-		if (n == 9)
-		{ 
-			printf("Сумма 10 членов ряда = %10.7lf\n", sum);
+		if (n == 9){
+			printf("Сумма 10 членов ряда = %10.7lf\n", sum);  // -2,64166
 		}
 	}
 	printf("Полная сумма ряда = %10.7lf\n", sum);
-	// -2,64166
-
 
 return 0;
 }
