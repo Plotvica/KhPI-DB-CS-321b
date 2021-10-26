8efa1c156b2e69dcaa3827e522e3fae9cd46437d
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-10-27 00:30:45 +0300

    9

---
+++ Laboratory Works/Laboratory Work 6A/Laboratory Work 6A.cpp
@@ -0,0 +1,45 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h";
+#include <time.h>;
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    srand(time(NULL));
+    int bot_num = rand() % 100 + 1;
+
+    int win, lost, user_num, attempts=0;
+
+	printf("Угадай число!\n");
+
+    do {
+			attempts++;
+        	scanf("%i", &user_num);           
+    	if (user_num > bot_num) {
+    		printf("Этот число больше моего.\n");
+            printf("Попробуй еще раз.\n");
+    	} 
+    	else if (user_num < bot_num) {
+			printf("Этот число меньше моего.\n");
+            printf("Попробуй еще раз.\n");
+    	}
+    	else {
+			printf("Поздравляю, ты угадал!");
+            printf("Это было %i \n\n", bot_num);
+    	}
+
+    } while(user_num!=bot_num);
+
+
+   	printf("Количество попыток:%i\n\n\n", attempts);
+
+
+return 0;
+}
+
+
---
+++ Laboratory Works/Laboratory Work 6B/Laboratory Work 6B.cpp
@@ -0,0 +1,43 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h";
+#include <time.h>;
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("Тест на знание таблици умножения:\n");
+
+	srand(time(NULL));
+   // int random_num1 = rand() % 10 + 1;
+   // int random_num2 = rand() % 10 + 1;
+   //int question = random_num1*random_num2;
+
+    int in_put_user_num, grade = 0, num_of_question;
+
+    for (num_of_question = 1; num_of_question <= 10; num_of_question++) {
+
+        int random_num1 = rand() % 10 + 1;
+        int random_num2 = rand() % 10 + 1;
+        int question = random_num1 * random_num2;
+
+    	printf("%i): %i * %i\n", num_of_question, random_num1, random_num2);
+        scanf ("%i", &in_put_user_num);
+
+    	if(question!=in_put_user_num){
+    		printf("Ошибка! Правильний ответ: %i\n", question);
+    	} else 
+    		grade++;
+    }
+
+	printf("Твой общий результат набранных баллов:%i\n", grade);
+
+			
+
+return 0;
+}
+
