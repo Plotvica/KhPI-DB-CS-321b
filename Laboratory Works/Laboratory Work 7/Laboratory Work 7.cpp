8c02fa5720a14af47d823c83d17dfab2c7ba967b
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-11-03 01:47:34 +0200

    4

---
+++ Laboratory Works/Laboratory Work 7/Laboratory Work 7.cpp
@@ -0,0 +1,51 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h"
+
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    short n;
+    double x, y;
+    short h;
+    float temp = 0;
+
+    for (n = 0; n < 5; n++) {
+
+        printf("|   x   |      y     |\n");
+        printf("|-------|------------|\n");
+
+        if (n == 0){
+            x = 0;
+        }
+        else{
+            x = -1;
+        }
+
+        for (; x <= 3; x += 0.25) {
+            if (x <= 3) {
+                y = -abs(x - 1) + 1;
+            }
+            
+            printf("| %5.2lg | %10.7lg |", x + n * 5, y);
+
+            h = (y + 1) * 8;  
+
+            if (y - 1 - h * 8 > 0.25) { 
+                h++;
+            };
+
+            for (; h > 0; h--){
+               printf(" ");
+            };
+
+            printf("*\n");
+        }
+        printf("Нажмите клавишу Enter...");
+        getchar();
+    }
+    return 0;
+}
