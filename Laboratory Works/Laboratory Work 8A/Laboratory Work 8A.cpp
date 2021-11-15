f8e88530d1f5da52bb7b593ee5f23817cea9d808
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-11-16 00:07:06 +0200

    2

--- Laboratory Works/Laboratory Work 8A/Laboratory Work 8A.cpp
+++ Laboratory Works/Laboratory Work 8A/Laboratory Work 8A.cpp
@@ -5,22 +5,60 @@
 #include <ctime>
 #include <iomanip>
 #include <iostream>
-
-
 #include "windows.h";
 using namespace std;
 
-int main() 
-{   
+int main()
+{
     int const SIZE = 5;
-    int smoking_space[SIZE];
-    int no_smoking_space[SIZE];
+    int smoking_space[SIZE] = { 1, 2, 3, 4, 5 };
+    int no_smoking_space[SIZE] = { 6, 7, 8, 9, 10 };
 
-    for (i=0; i < SIZE + SIZE; i++){
-        cout << "Does passanger smoke? <Y/N>" << endl;
-        sin >> >> endl;
-        if ()
+
+    cout << "Welcome " << endl;
+    cout << "We have two sitings areas \"smoking space\" and no \"smoking space\". You need to chose one." << endl;
+
+
+    string answer;
+    string smoke("yes");
+    string no_smoke("no");
+
+    int book_break = 0;
+
+    while ( book_break < SIZE*2) {
+
+        cout << "Are you smoking?<yes/no>  ";
+        cin >> answer;
+
+        if (cin.fail()){
+            cin.clear();
+            //cin.ignore(10000, '\n');
+            continue;
+        }
+
+        if (answer == smoke) {
+            int i;
+            for ( i = 0; i < SIZE; i++) {
+              //  cout << " Place " << smoking_space[i] << " is booked!" << endl;
+            }
+            cout << " Place " << smoking_space[i] << " is booked!" << endl;
+            book_break++;
+        }
+
+        if (answer == no_smoke) {
+            int j;
+            for (j = 0; j < SIZE; j++) {
+              // cout << " Place " << no_smoking_space[j] << " is booked!" << endl;
+            }
+            cout << " Place " << no_smoking_space[j] << " is booked!" << endl;
+            book_break++;
+        }
+   
     }
+
+
+
+
 
     cout << "Program has been ended" << endl;
   return 0;  
--- Laboratory Works/Laboratory Work 8B/Laboratory Work 8B.cpp
+++ Laboratory Works/Laboratory Work 8B/Laboratory Work 8B.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <algorithm> 
 #include <iomanip>
 #include <string>
@@ -10,13 +10,25 @@ int main()
     int arr_size;
     cout << "Let`s crate an array.";
     cout << "How many element`s do you need? ";
-    cin >> arr_size;
+    while (true) {
+       cin >> arr_size;
+       if (cin.fail()) {
+           cin.clear();
+           cin.ignore(1000, '\n');
+           cout << "Invalid value! Pease try againg. ";
+       }
+       else break;
+    }
 
-    int* arr = new int[arr_size];
+    int *arr = new int[arr_size];
+
+    int k; // num of elemnts that we add or del
+    int i; // index of first array
+    int j; // index of second array
 
     // random created array
     cout << "Your array:" << endl;
-    for (int i = 0; i < arr_size; i++) {
+    for ( i = 0; i < arr_size; i++) {
         arr[i] = rand() % 10 + 1;
         cout << arr[i] << setw(4);
     }
@@ -26,93 +38,130 @@ int main()
     string answer;
     string del_new("del");
     string add_new("add");
+    while (true) {
+        cout << "Do you whant to delete element`s or add some new? <del/add> ";
+        cin >> answer;
 
-    cout << "Do you whant to delete element`s or add some new? <del/add> ";
-    cin >> answer;
+        // add k elements
+        if (answer == add_new) {
+            // input 
+            int pos;
+            cout << "How many elements do you whant to add? ";
+            while (true) {
+                cin >> k;
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << "Invalid value! Pease try againg. ";
+                }
+                else break;
+            }
 
-    int k; // num of elemnts that we add or del
-    int i; // index of array
-    int pos_rep = 0; // position repeat of index
+            cout << "Choose the position where you whant to add " << k << " elements ";
+            while (true) {
+                cin >> pos;
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << "Invalid value! Pease try againg. ";
+                }
+                else break;
+            }
 
-    // add k elements
-    if (answer == add_new) {
-        // input 
-        int pos;
-        cout << "How many elements do you whant to add? ";
-        cin >> k;
-        cout << "Choose the position where you whant to add " << k << " elements ";
-        cin >> pos;
+            if (pos < 1 || pos > arr_size) {
+                cout << "Error! You let the arry`s limit out." << endl;
+            }
 
-        if (pos < 1 || pos > arr_size) {
-            cout << "Error! You let the arry`s limit out." << endl;
+            // add loops
+            else {
+                int *add_new_arr = new int[arr_size + k];
+
+                for (i = 0; i < pos; i++) {
+                    add_new_arr[i] = arr[i];
+                }
+
+                int *add_elements = new int[k];
+
+                cout << "New elements: ";
+                for ( j = 0; j < k; j++) {
+                    add_elements[j] = rand() % 10 + 1;
+                    cout << add_elements[j] << setw(4);
+                }
+                cout << endl;
+
+                for ( j = 0, i = pos; i < arr_size + k; i++, j++) {
+                    add_new_arr[i] = add_elements[j];
+                }
+
+                for ( j = pos + k, i = pos; j < arr_size + k; j++, i++) {
+                    add_new_arr[i + k] = arr[i];
+                }
+
+                // new array
+                cout << "Your new array:" << endl;
+                for (i = 0; i < arr_size + k; i++) {
+                    cout << add_new_arr[i] << setw(4);
+                }
+                cout << endl;
+            }
         }
+        // del k elements
+        else if (answer == del_new) {
 
-        // add loops
-        else {
-            int* add_arr = new int[arr_size + k];
-
-            for (i = 0; i < pos; i++) {
-                add_arr[i] = arr[i];
+            // input 
+            cout << "How many elements do you whant to delete(left to right)? ";
+            while (true) {
+                cin >> k;
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << "Invalid value! Pease try againg. ";
+                }
+                else break;
             }
 
-            int* add_extra = new int[k];
-
-            cout << "New elements: ";
-            for (int j = 0; j < k; j++) {
-                add_extra[j] = rand() % 10 + 1;
-                cout << add_extra[j] << setw(4);
-            }
-            cout << endl;
-
-            for (int j = 0, i = pos; i < arr_size + k; i++, j++) {
-                add_arr[i] = add_extra[j];
+            cout << "Choose the position where you whant to delete " << k << " elements ";
+            while (true) {
+                cin >> i;
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << "Invalid value! Pease try againg. ";
+                }
+                else break;
             }
 
-            for (int j = pos + k, i = pos; j < arr_size + k; j++, i++) {
-                add_arr[i + k] = arr[i];
+            // delet loop
+            if (i + k > arr_size or i < 0 or i > arr_size) {
+                cout << "Error! You let the array`s limit out." << endl;
             }
 
-            // new array
-            cout << "Your new array:" << endl;
-            for (i = 0; i < arr_size + k; i++) {
-                cout << add_arr[i] << setw(4);
+            else if (arr_size == k and i == 0) {
+                cout << "Array has been deleted!" << endl;
             }
-            cout << endl;
+
+            else {
+
+                for (; i < arr_size - k; i++) {
+                    arr[i] = arr[i + k];
+                }
+
+                // new array
+                cout << "Your new array:" << endl;
+                for (i = 0; i < arr_size - k; i++) {
+                    cout << arr[i] << setw(4);
+                }
+                cout << endl;
+            }
         }
+        cout << endl;
+        cout << "That`s new one:" << endl;
+        for (int i = 0; i < arr_size; i++) {
+            arr[i] = rand() % 10 + 1;
+            cout << arr[i] << setw(4);
+        }
+        cout << endl;
     }
-    // del k elements
-    else if (answer == del_new) {
-
-        // input 
-        cout << "How many elements do you whant to delete(left to right)? ";
-        cin >> k;
-        cout << "Choose the position where you whant to delete " << k << " elements ";
-        cin >> i;
-
-        // delet loop
-        if (i + k > arr_size || i < 0 || i > arr_size) {
-            cout << "Error! You let the array`s limit out." << endl;
-        }
-
-        else if (arr_size == k && i == 0) {
-            cout << "Array has been deleted!" << endl;
-        }
-
-        else {
-
-            for (; i < arr_size - k; i++) {
-                arr[i] = arr[i + k];
-            }
-
-            // new array
-            cout << "Your new array:" << endl;
-            for (i = 0; i < arr_size - k; i++) {
-                cout << arr[i] << setw(4);
-            }
-            cout << endl;
-        }
-    }
-
     return 0;
 }
 
