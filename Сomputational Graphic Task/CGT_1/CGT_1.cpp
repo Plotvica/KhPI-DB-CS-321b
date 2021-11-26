d9a05b9ee32b57cd9533b7ee303da50ac842be64
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-11-26 22:56:10 +0200

    2

--- Сomputational Graphic Task/CGT_1/CGT_1.cpp
+++ Сomputational Graphic Task/CGT_1/CGT_1.cpp
@@ -2,57 +2,39 @@
 #include<ctime>
 #include<cmath>
 #include<iomanip>
+#include <chrono>
 
 using namespace std;
 
-void Shuttle( int arr[], int size )
-{
-    
+
+void Shuttle(int arr[], int size)
+{  
     int j, i, temp;
 
-    long double time1 = clock() / 1000.0;
-
-    for (i = 1; i < size; i++) {    
+    for (i = 1; i < size; i++) {
 
         j = i - 1;
 
-        while(j >= 0){
+        while (j >= 0) {
 
-            if( arr[j] > arr[j + 1] ){ 
-
+            if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 j--;
-
             }
             else break;
-
         }
-
     }
-
-
-    long double time2 = clock() / 1000.0;
-
-   
-    cout << "Sorted array:" << endl;
-    for (int i = 0; i < size; i++) {
-        cout << setw(3) << arr[i];
-    } 
-
-   /* for (auto i: arr) {
-        cout << arr[i];
-    } 
-    cout << endl;*/
-
-    cout << "\nTime : " << time2 - time1 << endl;
 }
 
 
 int main()
 {
     while (true) {
+
+        cout.setf(ios::fixed);
+        cout.precision(15);
 
         cout << "Shuttle sort" << endl;
         int size;
@@ -62,17 +44,46 @@ int main()
         int arr[1000];
 
         cout << "Unsorted array:" << endl;
+        for (int i = 0; i < size; i++) {
+
+           //arr[i] = rand() % 10 + 1;
+           arr[i] = i;
+           cout << setw(3) << arr[i];
+
+        }
+
+        double aver_dur = 0;
+       
+        double dur_arr[1000];
+
         for (int i = 0; i < size; i++){
-            arr[i] = rand() % 10 + 1;
-            cout << setw (3) <<  arr [i];
+
+            auto begin = std::chrono::high_resolution_clock::now();
+ 
+            Shuttle(arr, size);
+
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duration = end - begin;
+
+            dur_arr[i]= duration.count();
+        }
+
+        for (int i = 0; i < size; i++){
+
+            if (i > 0) aver_dur += dur_arr[i];
+   
+        }
+
+        cout << "\nSorted array:" << endl;
+        for (int i = 0; i < size; i++) {
+
+             cout << setw(3) << arr[i];
+
         } 
-        cout << endl;
 
-        Shuttle(arr, size);
-
+        std::cout << "\nThe time:" << aver_dur / (size - 1) << "s\n" << endl;   
     }
 
-
-    system("pause");
+    //system("pause");
     return 0;
 }
--- Сomputational Graphic Task/CGT_2/CGT_2.cpp
+++ Сomputational Graphic Task/CGT_2/CGT_2.cpp
@@ -1,16 +1,15 @@
 ﻿#include<iostream> 
 #include<ctime>
 #include<cmath>
-
+#include<iomanip>
+#include <chrono>
 
 using namespace std;
-
 
 
 void Shell(int arr[], int size)
 {
     int d = size / 2;
-    long double time1 = clock() / 1000.0;
 
     while (d > 0)
     {
@@ -22,16 +21,11 @@ void Shell(int arr[], int size)
                 int temp = arr[j];
                 arr[j] = arr[j + d];
                 arr[j + d] = temp;
-
                 j--;
             }
         }
         d = d / 2;
     }
-
-    long double time2 = clock() / 1000.0;
-
-    cout << "\nTime : " << time2 - time1 << endl;
 }
 
 
@@ -39,21 +33,57 @@ int main()
 {
     while (true) {
 
+        cout.setf(ios::fixed);
+        cout.precision(15);
+
+        cout << "Shell sort" << endl;
         int size;
-        cout << "TEST ";
+        cout << "Input array size: ";
         cin >> size;
 
-        int *arr = new int[size];
+        int arr[1000];
 
-        for (int i = 0; i < size; i++){
-            arr[i] = rand() % 10 + 1;
+        cout << "Unsorted array:" << endl;
+        for (int i = 0; i < size; i++) {
+
+          // arr[i] = rand() % 10 + 1;
+           arr[i] = i;
+           cout << setw(3) << arr[i];
+
         }
 
-        Shell(arr, size);
+        double aver_dur = 0;
+       
+        double dur_arr[1000];
 
+        for (int i = 0; i < size; i++){
+
+            auto begin = std::chrono::high_resolution_clock::now();
+ 
+            Shell(arr, size);
+
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> duration = end - begin;
+
+            dur_arr[i]= duration.count();
+        }
+
+        for (int i = 0; i < size; i++){
+
+            if (i > 0) aver_dur += dur_arr[i];
+   
+        }
+
+        cout << "\nSorted array:" << endl;
+        for (int i = 0; i < size; i++) {
+
+             cout << setw(3) << arr[i];
+
+        } 
+
+        std::cout << "\nThe time:" << aver_dur / (size - 1) << "s\n" << endl;   
     }
 
-
-    system("pause");
+    //system("pause");
     return 0;
 }
