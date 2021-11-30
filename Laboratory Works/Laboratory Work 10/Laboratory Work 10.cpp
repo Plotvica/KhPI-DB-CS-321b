fef191bf3d26aa7319d570a642e8086a1c19c569
Plotvica <90572760+Plotvica@users.noreply.github.com>
2021-11-30 20:10:44 +0200

    1

--- Laboratory Works/Laboratory Work 10/Laboratory Work 10.cpp
+++ Laboratory Works/Laboratory Work 10/Laboratory Work 10.cpp
@@ -15,6 +15,7 @@ int matrix_style (int matrix[100][100], int row_col, int array[100],int total_su
          cout << setw(4) << matrix[i][j];
         }cout << endl;
     } 
+
     //avarage value
     for (int i = 0; i < row_col; ++i){
 
@@ -31,14 +32,26 @@ int matrix_style (int matrix[100][100], int row_col, int array[100],int total_su
         total_sum += avarage_value;
         avarage_value = 0;
     } 
-        cout << "New multipled matrix:" << endl;
-        for (int i = 0; i < row_col; ++i){
-            for (int j = 0; j < row_col; ++j){
-             cout << setw(4) << matrix[i][j];
-            }cout << endl;
-        } 
 
-    cout << "\nTest:" << total_sum << endl;
+    // output matrix
+    cout << "New multipled matrix:" << endl;
+      for (int i = 0; i < row_col; ++i){
+          for (int j = 0; j < row_col; ++j){
+           cout << setw(4) << matrix[i][j];
+          }cout << endl;
+      } 
+
+    // back frist form array
+    cout << "New array:" << endl;
+     for (int i = 0; i < row_col; ++i) {
+         for (int j = 0; j < row_col; ++j) {
+             array[(i + j)] = matrix[i][j];
+             cout << setw(3) << array[(i + j)];
+         }
+     }
+
+        
+    cout << "\nTotal summa of avarage is:";
     return total_sum;
 }
 
@@ -50,35 +63,27 @@ int main()
 
         int array[100];
         int matrix[100][100];
+       
         // input size of arry 
         int size_array;
-        int total_sum = 0;
-        cout << "Input array size multiple of the root size of the array" << endl;
+        cout << "Input array size multiple of the root size of the array ";
         cin >> size_array;
        
        if (sqrt(size_array) - int(sqrt(size_array)) == 0){
+
             // array output
-            cout << "Your array is:" << endl;
-            
+           cout << "Your array is:" << endl;
            for (int i = 0; i < size_array; ++i){
                 array[i] = rand()%10 + 1;
                 cout << setw(3) <<array[i];
            } int row_col = sqrt(size_array);
 
-           matrix_style( matrix, row_col, array, total_sum);
-
-           cout << "Newarra" << endl;
-           for (int i = 0; i < row_col; ++i) {
-               for (int j = 0; j < row_col; ++j) {
-                   array[(i + j)] = matrix[i][j];
-                   cout << setw(3) << array[(i + j)];
-               }
-           }
-           //cout  <<"\nTest:" << total_sum;
+           int total_sum = 0;
+           cout << matrix_style(matrix, row_col, array, total_sum) << endl;
 
        } else cout << "Error" << endl;
 
-       cout << "\n\n\n";
+       cout << "\n\n";
 
    }
 
